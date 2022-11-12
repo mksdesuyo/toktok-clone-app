95274d68cdc93b58dc27a56afbf98e673c68f3bf
RizqiMKS <rizqimks@gmail.com>
2022-11-12 14:46:01 +0700

    Changing on next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: [
       'avatars.githubusercontent.com', 
